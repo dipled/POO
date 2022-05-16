345df5fb4468a5f61d18f9f0b9d32e343866149d
pe91 <87032838+pe91@users.noreply.github.com>
2022-05-16 16:24:14 -0300

    Update Digrafo.java

--- Lista3/ex3/src/dados/Digrafo.java
+++ Lista3/ex3/src/dados/Digrafo.java
@@ -1,17 +1,31 @@
 package dados;
 import java.util.List;
+
+import javax.sound.sampled.SourceDataLine;
+
 import java.util.LinkedList;
 
 public class Digrafo{
     protected List<List<Integer>> matrizAdjacendia1 = new LinkedList<>();
     
 
-
     public void adicionarVertice(){
-        for(int i = 0; i < matrizAdjacendia1.size() + 1; i += 1){
-            for(int i2 = matrizAdjacendia1.get(i).size(); i2 < matrizAdjacendia1.get(i).size() + 1; i2 += 1){
-                matrizAdjacendia1.get(i).add(0);
-            }
+
+        List<Integer> novaLinha  = new LinkedList<>();
+        for(int i = 0; i < matrizAdjacendia1.size()+1; i += 1){
+            novaLinha.add(0);
+        }
+        this.matrizAdjacendia1.add(novaLinha);
+        for(int i = 0; i < matrizAdjacendia1.size()+1; i += 1){
+            matrizAdjacendia1.get(i).add(0);
+        }
+        
+    }
+    public void adicionarAresta(int origem, int destino){
+        matrizAdjacendia1.get(origem).set(destino, 1);
+    }
+    public String toString(){
+        
         }
     }
 }
